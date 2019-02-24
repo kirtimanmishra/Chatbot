91c2103d016c8d6045259dd8f7e7b23eaf2d81bc
sathwikmatsa <sathwikmatsa@gmail.com>
2019-02-24 23:08:37 +0530

    fix class name collisions

--- build/chatbot.js
+++ build/chatbot.js
@@ -1,18 +1,18 @@
 let chatbot_url = "https://cdn.jsdelivr.net/gh/sathwikmatsa/Chatbot@master";
 let SERVER_URL = "http://localhost:5000";
 let chatbot_code = `
-    <div id="chatbox">
-      <div id="chatheader" class="center">
-          <img src="${chatbot_url}/assets/chatbot_avatar.png" class="avatar" alt="Avatar">
-          <img src="${chatbot_url}/assets/toggle.png" id="toggler" class="toggler" alt="toggler">
+    <div id="chatbox_iittp">
+      <div id="chatheader_iittp" class="center-iittp">
+          <img src="${chatbot_url}/assets/chatbot_avatar.png" class="avatar-iittp" alt="Avatar">
+          <img src="${chatbot_url}/assets/toggle.png" id="toggler_iittp" class="toggler-iittp" alt="toggler">
       </div>
-      <div id=toggle>
-      <div id="chat">
+      <div id="toggle">
+      <div id="chat_iittp">
 
       </div>
-      <div id="send_message" class="center">
-          <textarea id="message" placeholder="Type a message..."></textarea>
-          <input type="image" src="https://cdn.jsdelivr.net/gh/sathwikmatsa/Chatbot@master/assets/send.png" id="send" alt="submit">
+      <div id="send_message_iittp" class="center">
+          <textarea id="message_iittp" placeholder="Type a message..."></textarea>
+          <input type="image" src="https://cdn.jsdelivr.net/gh/sathwikmatsa/Chatbot@master/assets/send.png" id="send_iittp" alt="submit">
       </div>
       </div>
   </div>`;
@@ -29,20 +29,20 @@ function send_response(text){
     xmlhttp.send();
     if (xmlhttp.status === 200) {
         result = xmlhttp.responseText;
-        create_chat_item(result, "bot");
+        create_chat_item(result, "bot-iittp");
     }
 }
 
 function create_chat_item(text, cls){
     let html_template = `
-        <div class="conversation `+ cls +`">
-            <p class="text">`+ text +`</p>
+        <div class="conversation-iittp `+ cls +`">
+            <p class="text-iittp">`+ text +`</p>
         </div>
     `;
     let chat_message = createElementFromHTML(html_template);
-    let chat = document.getElementById('chat');
+    let chat = document.getElementById('chat_iittp');
     chat.appendChild(chat_message);
-    if(cls == "human"){
+    if(cls == "human-iittp"){
         send_response(text);
     }
     chat.scrollTop = chat.scrollHeight;
@@ -52,18 +52,18 @@ function create_chat_item(text, cls){
 (function(){
     document.body.appendChild(createElementFromHTML(chatbot_code));
     document.head.appendChild(createElementFromHTML('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sathwikmatsa/Chatbot@master/chatbot_styles.css">'));
-    document.getElementById('chatheader').addEventListener("click", function(){
-        let toggle = document.getElementById('toggle');
+    document.getElementById('chatheader_iittp').addEventListener("click", function(){
+        let toggle = document.getElementById('toggle_iittp');
         if(toggle.style.display == "none"){
             toggle.style.display = "block";
         } else {
             toggle.style.display = "none";
         }
     });
-    document.getElementById('send').addEventListener("click", function(){
-        let message_box = document.getElementById('message');
+    document.getElementById('send_iittp').addEventListener("click", function(){
+        let message_box = document.getElementById('message_iittp');
         if(message_box.value){
-            create_chat_item(message_box.value, "human");
+            create_chat_item(message_box.value, "human-iittp");
             message_box.value = ""
         }
     });
