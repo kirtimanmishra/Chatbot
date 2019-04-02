605a3391c2404e34a6d8893ecb14494a6b625903
saiNitish <bsainitishkumar@gmail.com>
2019-04-02 23:37:46 +0530

    added dialog generation

---
+++ test2.py
@@ -0,0 +1,41 @@
+import json
+import itertools
+
+f = open("states.json")
+data = json.load(f)
+# fwrite = open("test.txt",'w')
+variables = []
+
+temp = sorted(data.items(), key = lambda k:k[1]["priority"])
+dic = {}
+for i in temp:
+	dic[i[0]] = i[1]
+
+data = dic
+
+for k in data:
+	variables.append({"name":k,"data":data[k]})
+	temp = data[k]
+
+lis = []
+for i in variables:
+	if(i["data"]["type"]=="bool"):
+		lis.append(i["data"]["val"])
+	elif(i["data"]["type"]=="range"):
+		lis.append(i["data"]["val"])
+	elif(i["data"]["type"]=="string"):
+		lis.append([i["data"]["val"]])
+	elif(i["data"]["type"]=="number"):
+		lis.append([i["data"]["val"]])
+	elif(i["data"]["type"]=="str_list"):
+		lis.append(i["data"]["val"])
+
+lis = list(itertools.product(*lis))
+for j in range(len(lis)):
+	print("Dialog {}".format(j))
+	for i in range(len(variables)):
+		print(variables[i]["name"]+"?")
+		print(lis[j][i])
+	print("##########\n")
+
+# fwrite.close()
